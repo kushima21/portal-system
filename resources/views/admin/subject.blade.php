e4142e3b7137a450f3184904d63e9ffa80a3709f
John Mark Hondrada <venomhondrada11@gmail.com>
2026-03-07 22:35:00 +0800

    john mark hondrada

--- resources/views/admin/subject.blade.php
+++ resources/views/admin/subject.blade.php
@@ -1,4 +1,16 @@
 @extends('layout.default')
 
 @section('content')
+    <div class="w-full h-full">
+        <div class="w-full h-[20%] fixed bg-amber-300">
+            <div class="flex items-center gap-3 p-5">
+                <span>Admin</span>
+                <span>></span>
+                <span>Manage</span>
+                <span>></span>
+                <span>Subjects</span>
+            </div>
+            <div class=""></div>
+        </div>
+    </div>
 @endsection
