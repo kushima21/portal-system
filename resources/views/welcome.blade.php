07de7bc85c1b9fcc2d816adfb27f1d81492df06a
John Mark Hondrada <venomhondrada11@gmail.com>
2026-03-07 20:14:47 +0800

    john mark hondrada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/css/app.css'])
 </head>
 <body>
-    <div class="w-full h-full bg-white">
-        <div class="w-full h-full flex">
-            <div class="w-[50%] h-full">
-                
-            </div>
+
+<div class="w-full h-screen bg-white">
+    <div class="w-full h-full flex">
+        
+        <div class="w-[50%] h-full flex items-center justify-center">
+            <div class="w-[80%] h-full bg-white shadow-lg"></div>
         </div>
+
+        <div class="w-[50%] h-full"></div>
+
     </div>
+</div>
+
 </body>
 </html>
